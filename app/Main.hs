fae72324d675da2ed901472b53fb003981780262
Paul David <paul@denknerd.org>
2017-09-21 21:14:32 +1000

    Polish the CLI somewhat, name functions sensibly.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,28 @@
 module Main where
 
 import Lib
+import Console.Options
 
 main :: IO ()
-main = someFunc
+main = defaultMain $ do
+  programName "forest-fire"
+  programDescription "Recursively delete CFn dependencies"
+  flagA    <- flag $ FlagLong "delete"
+  allArgs  <- remainingArguments "FILE"
+  action $ \toParam -> do
+      let reallyDelete = toParam flagA
+      let stackName    = toParam allArgs
+      case stackName of
+        [s] -> if reallyDelete
+               then actuallyDoTheDelete s
+               else showDeletionPlan s
+        _   -> printUsage
+
+printUsage = do
+  putStrLn "\nforest-fire"
+  putStrLn "-----------\n"
+  putStrLn "Usage:\n"
+  putStrLn "  forest-fire <stackname> [--delete]\n"
+  putStrLn "Flags:\n"
+  putStrLn "--delete       To actually perform the deletion."
+  putStrLn ""
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,13 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 
+-- TODO
+-- * test suite (around JSON parsing, mostly)
+-- * modularise once boundaries are clear
+-- * clean up code a bit...
+-- * readme (w/ how to install and use)
+-- * send to Hackage/Stackage
+
 module Lib where
 
 import Data.Tree
@@ -17,48 +24,37 @@ import GHC.IO.Exception
 
 import Data.Aeson.Types    -- that's where Parser comes from
 
-someFunc :: IO ()
-someFunc = do
-  -- let candidate = StackName "kubernetes-dynamic-91acf0ef-lifecycle"
-  let candidate = StackName "kubernetes-dynamic-91acf0ef-controller-dns"
-  putStrLn $ "Retrieving dependencies of " ++ show candidate ++ "..."
-  tree <- buildTree candidate
+showDeletionPlan :: String -> IO ()
+showDeletionPlan stackName = do
+  putStrLn $ "Retrieving dependencies of " ++ stackName ++ "..."
+  tree <- buildTree (StackName stackName)
   -- let tree = exampleDep2
   putStrLn "Done.  Delete these stacks in postorder traversal:\n"
   putStrLn $ drawTree (dependencyToTree tree)
-  putStrLn "This would be postorder:\n"
+  putStrLn "Or, delete manually in this order:\n"
   mapM_ putStrLn $ postorder (dependencyToTree tree)
-  -- res <- mapM (executeEcho . singleton) $ postorder (dependencyToTree tree)
-  -- mapM_ putStrLn res
 
-tryDelete :: IO ()
-tryDelete = do
-  let candidate = StackName "kubernetes-dynamic-91acf0ef-controller-dns"
-  putStrLn $ "Retrieving dependencies of " ++ show candidate ++ "..."
-  tree <- buildTree candidate
-  -- let tree = exampleDep2
-  -- putStrLn "Done.  Delete these stacks in postorder traversal:\n"
-  -- putStrLn $ drawTree (dependencyToTree tree)
-  putStrLn "Deleting..."
-  -- mapM_ putStrLn $ postorder (dependencyToTree tree)
+actuallyDoTheDelete :: String -> IO ()
+actuallyDoTheDelete stackName = do
+  putStrLn $ "Retrieving dependencies of " ++ stackName ++ "..."
+  tree <- buildTree (StackName stackName)
+  putStrLn "Deleting dependencies and stack..."
   mapM_ (doDeletionWait . StackName) $ postorder (dependencyToTree tree)
-  -- mapM_ putStrLn res
 
--- singleton = pure -- is actually pure for List Monad
+data Dependency = Dependency {dStackName    :: StackName,
+                              dDependencies :: [Dependency]} deriving (Show)
 
-data Dependency = Dependency {name :: StackName,
-                              deps :: [Dependency]} deriving (Show)
-data Stack = Stack {sStackId :: StackId,
+data Stack = Stack {sStackId   :: StackId,
                     sStackName :: StackName,
-                    sExports :: [Export]} deriving Show
+                    sExports   :: [Export]} deriving Show
 
-newtype StacksResult = StacksResult [Stack] deriving Show
-newtype Export       = Export ExportName deriving Show
+newtype StacksResult = StacksResult { srStacks :: [Stack]} deriving Show
+newtype Export       = Export       { eName :: ExportName} deriving Show
 
-newtype StackName  = StackName  String deriving (Show, Generic, Eq)
-newtype StackId    = StackId    String deriving (Show, Generic, Eq)
-newtype ExportName = ExportName String deriving (Show, Generic)
-newtype Imports    = Imports    [StackName] deriving (Show)
+newtype StackName  = StackName  String      deriving (Show, Generic, Eq)
+newtype StackId    = StackId    String      deriving (Show, Generic, Eq)
+newtype ExportName = ExportName String      deriving (Show, Generic)
+newtype Imports    = Imports    { iStackNames :: [StackName] } deriving (Show)
 
 executeEcho :: [String] -> IO String
 executeEcho args = do
@@ -69,20 +65,22 @@ executeEcho args = do
 
 doDeletionWait :: StackName -> IO ()
 doDeletionWait (StackName s) = do
-  putStrLn $ "Deleting " ++ s ++ "."
+  putStrLn $ "Issuing delete command on stack " ++ s ++ "."
   res <- executeAWScommand False ["cloudformation", "delete-stack", "--stack-name", s]
   putStrLn "Awaiting completion... (60 minute timeout)"
   res <- executeAWScommand False ["cloudformation", "wait", "stack-delete-complete", "--no-paginate", "--stack-name", s]
-  putStrLn $ "Delete of " ++ s ++ " complete."
+  putStrLn $ "Removal of " ++ s ++ " complete."
 
 executeAWScommand :: Bool -> [String] -> IO B.ByteString
 executeAWScommand allowFail args = do
+  putStrLn $ "[EXEC] aws " ++ unwords args
   (code, stdout, stderr) <- readProcessWithExitCode "aws" args ""
   case code of
     ExitSuccess -> pure (B.pack stdout)
     _ -> if allowFail
          then pure (B.pack "")
-         else error (show stderr)
+         else do putStrLn stderr
+                 error $ show code
 
 jsonForDescribeStacks :: StackName -> IO B.ByteString
 jsonForDescribeStacks (StackName s) =
@@ -123,12 +121,12 @@ findExports :: StackName -> IO [ExportName]
 findExports = generalise
      jsonForDescribeStacks
      error
-     (\ (StacksResult ps) -> pure $ map (\ (Export e) -> e) $ concatMap sExports ps)
+     (pure . map eName . concatMap sExports . srStacks)
 
 findImports :: ExportName -> IO Imports
 findImports = generalise
     jsonForListImports
-    (\ _ -> pure (Imports []))
+    (const $ pure (Imports []))
     pure
 
 -- findExports :: StackName -> IO [ExportName]
@@ -138,9 +136,6 @@ generalise whence leftThing rightThing s = do
     Left err -> leftThing err
     Right ps -> rightThing ps
 
-getImport :: Imports -> [StackName]
-getImport (Imports i) = i
-
 buildTree :: StackName -> IO Dependency
 buildTree = buildTree' []
 
@@ -148,17 +143,15 @@ buildTree' :: [StackName] -> StackName -> IO Dependency
 buildTree' alreadySeen name = do
   outputs <- findExports name
   imports <- mapM findImports outputs
-  let downstreams = nub $ filter (`notElem` alreadySeen) $ concatMap getImport imports
+  let downstreams = nub $ filter (`notElem` alreadySeen) $ concatMap iStackNames imports
   downstreamDeps <- mapM (buildTree' (alreadySeen ++ downstreams)) downstreams
   pure $ Dependency name downstreamDeps
 
 
-exampleDep2 = Dependency {name = StackName "kubernetes-staging-controller", deps = [Dependency {name = StackName "kubernetes-staging-controller-dns", deps = [Dependency {name = StackName "kubernetes-staging-worker", deps = [Dependency {name = StackName "kubernetes-staging-worker-dns", deps = [Dependency {name = StackName "samson-dns-staging", deps = []},Dependency {name = StackName "treasure-hunt-dynamic-df11859c-dns", deps = []},Dependency {name = StackName "treasure-hunt-staging-dns", deps = []}]}]}]},Dependency {name = StackName "fluentd-shiny-staging-iam", deps = []},Dependency {name = StackName "kubernetes-staging-logging-bucket", deps = []}]}
+exampleDep2 = Dependency {dStackName = StackName "kubernetes-staging-controller", dDependencies = [Dependency {dStackName = StackName "kubernetes-staging-controller-dns", dDependencies = [Dependency {dStackName = StackName "kubernetes-staging-worker", dDependencies = [Dependency {dStackName = StackName "kubernetes-staging-worker-dns", dDependencies = [Dependency {dStackName = StackName "samson-dns-staging", dDependencies = []},Dependency {dStackName = StackName "treasure-hunt-dynamic-df11859c-dns", dDependencies = []},Dependency {dStackName = StackName "treasure-hunt-staging-dns", dDependencies = []}]}]}]},Dependency {dStackName = StackName "fluentd-shiny-staging-iam", dDependencies = []},Dependency {dStackName = StackName "kubernetes-staging-logging-bucket", dDependencies = []}]}
 
 dependencyToTree :: Dependency -> Tree String
 dependencyToTree (Dependency (StackName name) deps) = Node name (map dependencyToTree deps)
-
-asdf = putStrLn $ drawTree (dependencyToTree exampleDep2)
 
 postorder :: Tree a -> [a]
 postorder (Node label kids) = concatMap postorder kids ++ [label]
