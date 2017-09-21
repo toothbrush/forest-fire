a189860e3034df835b5c8e9fbbd6f8bd536fff1d
Paul David <paul@denknerd.org>
2017-09-21 18:35:10 +1000

    Add all basic functionality.

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,130 +14,151 @@ import GHC.Generics
 import System.Process
 import Data.Maybe
 import GHC.IO.Exception
--- Vector is the type Aeson uses to represent JSON arrays
-import qualified Data.Vector as V
-import qualified Data.Text.Lazy.IO as T
-import qualified Data.Text.Lazy.Encoding as T
 
 import Data.Aeson.Types    -- that's where Parser comes from
 
 someFunc :: IO ()
 someFunc = do
-  tree <- buildTree (StackName "kubernetes-staging-controller")
-  putStrLn $ show tree
+  -- let candidate = StackName "kubernetes-dynamic-91acf0ef-lifecycle"
+  let candidate = StackName "kubernetes-dynamic-91acf0ef-controller-dns"
+  putStrLn $ "Retrieving dependencies of " ++ show candidate ++ "..."
+  tree <- buildTree candidate
+  -- let tree = exampleDep2
+  putStrLn "Done.  Delete these stacks in postorder traversal:\n"
+  putStrLn $ drawTree (dependencyToTree tree)
+  putStrLn "This would be postorder:\n"
+  mapM_ putStrLn $ postorder (dependencyToTree tree)
+  -- res <- mapM (executeEcho . singleton) $ postorder (dependencyToTree tree)
+  -- mapM_ putStrLn res
+
+tryDelete :: IO ()
+tryDelete = do
+  let candidate = StackName "kubernetes-dynamic-91acf0ef-controller-dns"
+  putStrLn $ "Retrieving dependencies of " ++ show candidate ++ "..."
+  tree <- buildTree candidate
+  -- let tree = exampleDep2
+  -- putStrLn "Done.  Delete these stacks in postorder traversal:\n"
+  -- putStrLn $ drawTree (dependencyToTree tree)
+  putStrLn "Deleting..."
+  -- mapM_ putStrLn $ postorder (dependencyToTree tree)
+  mapM_ (doDeletionWait . StackName) $ postorder (dependencyToTree tree)
+  -- mapM_ putStrLn res
+
+-- singleton = pure -- is actually pure for List Monad
 
 data Dependency = Dependency {name :: StackName,
                               deps :: [Dependency]} deriving (Show)
-
-newtype StackName  = StackName  String deriving (Show, Generic, Eq)
-newtype StackId    = StackId    String deriving (Show, Generic, Eq)
-newtype ExportName = ExportName String deriving (Show, Generic)
-
-findExportsJSON :: StackName -> IO String
-findExportsJSON (StackName s) = do
-  (code, stdout, stderr) <- readProcessWithExitCode "aws" ["cloudformation", "describe-stacks", "--stack-name", s] ""
-  case code of
-    ExitSuccess -> return stdout
-    _ -> error (show stderr)
-
 data Stack = Stack {sStackId :: StackId,
                     sStackName :: StackName,
                     sExports :: [Export]} deriving Show
 
-data StacksResult = StacksResult [Stack] deriving Show
-data Export = Export ExportName deriving Show
+newtype StacksResult = StacksResult [Stack] deriving Show
+newtype Export       = Export ExportName deriving Show
 
-dependents :: ExportName -> IO String
-dependents (ExportName e) = do
-  (code, stdout, stderr) <- readProcessWithExitCode "aws" ["cloudformation", "list-imports", "--export-name", e] ""
+newtype StackName  = StackName  String deriving (Show, Generic, Eq)
+newtype StackId    = StackId    String deriving (Show, Generic, Eq)
+newtype ExportName = ExportName String deriving (Show, Generic)
+newtype Imports    = Imports    [StackName] deriving (Show)
+
+executeEcho :: [String] -> IO String
+executeEcho args = do
+  (code, stdout, stderr) <- readProcessWithExitCode "echo" args ""
   case code of
-    ExitSuccess -> return stdout
-    _ -> return ""
-    -- _ -> error (show stderr)
+    ExitSuccess -> pure stdout
+    _ -> error (show stderr)
 
-data Imports = Imports [StackName]
-  deriving (Show, Generic)
+doDeletionWait :: StackName -> IO ()
+doDeletionWait (StackName s) = do
+  putStrLn $ "Deleting " ++ s ++ "."
+  res <- executeAWScommand False ["cloudformation", "delete-stack", "--stack-name", s]
+  putStrLn "Awaiting completion... (60 minute timeout)"
+  res <- executeAWScommand False ["cloudformation", "wait", "stack-delete-complete", "--no-paginate", "--stack-name", s]
+  putStrLn $ "Delete of " ++ s ++ " complete."
+
+executeAWScommand :: Bool -> [String] -> IO B.ByteString
+executeAWScommand allowFail args = do
+  (code, stdout, stderr) <- readProcessWithExitCode "aws" args ""
+  case code of
+    ExitSuccess -> pure (B.pack stdout)
+    _ -> if allowFail
+         then pure (B.pack "")
+         else error (show stderr)
+
+jsonForDescribeStacks :: StackName -> IO B.ByteString
+jsonForDescribeStacks (StackName s) =
+  executeAWScommand False ["cloudformation", "describe-stacks", "--stack-name", s]
+
+jsonForListImports :: ExportName -> IO B.ByteString
+jsonForListImports (ExportName e) =
+  -- this one's optional, don't fail.
+  executeAWScommand True ["cloudformation", "list-imports", "--export-name", e]
 
 instance FromJSON Export where
   parseJSON = withObject "output object" $ \t -> do
     exportName <- t .: "ExportName"
-    return (Export exportName)
+    pure (Export exportName)
 
 instance FromJSON StacksResult where
   parseJSON = withObject "stacks result object" $ \t -> do
     stacksArray <- t .: "Stacks"
-    return (StacksResult stacksArray)
+    pure (StacksResult stacksArray)
 
 instance FromJSON Stack where
   parseJSON = withObject "stack summary object" $ \t -> do
     stackid <- t .: "StackId"
     stackname <- t .: "StackName"
-    outputs <- t .:? "Outputs" .!= []
-    return (Stack stackid stackname outputs)
+    outputs <- t .:? "Outputs" .!= [] -- If there are no outputs, return []
+    pure (Stack stackid stackname outputs)
 
 instance FromJSON Imports where
   parseJSON = withObject "imports result object" $ \t -> do
     list <- t .: "Imports"
-    return (Imports list)
+    pure (Imports list)
 
 instance FromJSON StackName
 instance FromJSON StackId
 instance FromJSON ExportName
 
-getExportsJSON :: StackName -> IO B.ByteString
-getExportsJSON = fmap B.pack <$> findExportsJSON
-
-getJSON :: ExportName -> IO B.ByteString
-getJSON = fmap B.pack <$> dependents
-
 findExports :: StackName -> IO [ExportName]
-findExports s = do
-  sr <- (eitherDecode <$> getExportsJSON s) :: IO (Either String StacksResult)
-  case sr of
-    Left err -> error err
-    Right (StacksResult ps) -> pure $ map (\ (Export e) -> e) $ concatMap sExports ps
+findExports = generalise
+     jsonForDescribeStacks
+     error
+     (\ (StacksResult ps) -> pure $ map (\ (Export e) -> e) $ concatMap sExports ps)
 
 findImports :: ExportName -> IO Imports
-findImports e = do
-  d <- (eitherDecode <$> getJSON e) :: IO (Either String Imports)
-  case d of
-    Left err -> pure (Imports []) -- assume that empty aws feedback means nobody uses this Output
-    Right ps -> pure ps
+findImports = generalise
+    jsonForListImports
+    (\ _ -> pure (Imports []))
+    pure
+
+-- findExports :: StackName -> IO [ExportName]
+generalise whence leftThing rightThing s = do
+  sr <- eitherDecode <$> whence s --- :: IO (Either String StacksResult)
+  case sr of
+    Left err -> leftThing err
+    Right ps -> rightThing ps
 
 getImport :: Imports -> [StackName]
 getImport (Imports i) = i
 
--- EXAMPLES
-
--- find the stacks that import this value:
-tryit :: IO Imports
-tryit = findImports (ExportName "kubernetes-staging-security-groups-ControllerInternalLBSg")
-
--- find the outputs of that stack:
-outs :: IO [ExportName]
-outs = do
-  (Imports stacks) <- tryit
-  let [name] = stacks
-  findExports name
-
 buildTree :: StackName -> IO Dependency
-buildTree = buildTreeNoDup []
+buildTree = buildTree' []
 
-buildTreeNoDup :: [StackName] -> StackName -> IO Dependency
-buildTreeNoDup alreadySeen name = do
-  outputs <- findExports name -- :: [ExportName]
-  imports <- mapM findImports outputs -- :: [Imports]
-  let downstreams = nub $ filter (\sn -> not $ elem sn alreadySeen) $ concatMap getImport imports -- :: [StackName]
-  downstreamDeps <- (mapM (\x -> buildTreeNoDup (alreadySeen++downstreams) x) downstreams)
-  return $ Dependency name downstreamDeps
+buildTree' :: [StackName] -> StackName -> IO Dependency
+buildTree' alreadySeen name = do
+  outputs <- findExports name
+  imports <- mapM findImports outputs
+  let downstreams = nub $ filter (`notElem` alreadySeen) $ concatMap getImport imports
+  downstreamDeps <- mapM (buildTree' (alreadySeen ++ downstreams)) downstreams
+  pure $ Dependency name downstreamDeps
 
 
 exampleDep2 = Dependency {name = StackName "kubernetes-staging-controller", deps = [Dependency {name = StackName "kubernetes-staging-controller-dns", deps = [Dependency {name = StackName "kubernetes-staging-worker", deps = [Dependency {name = StackName "kubernetes-staging-worker-dns", deps = [Dependency {name = StackName "samson-dns-staging", deps = []},Dependency {name = StackName "treasure-hunt-dynamic-df11859c-dns", deps = []},Dependency {name = StackName "treasure-hunt-staging-dns", deps = []}]}]}]},Dependency {name = StackName "fluentd-shiny-staging-iam", deps = []},Dependency {name = StackName "kubernetes-staging-logging-bucket", deps = []}]}
 
-test :: Dependency -> Tree String
-test (Dependency (StackName name) deps) = Node name (map test deps)
+dependencyToTree :: Dependency -> Tree String
+dependencyToTree (Dependency (StackName name) deps) = Node name (map dependencyToTree deps)
 
-asdf = putStrLn $ drawTree (test exampleDep2)
+asdf = putStrLn $ drawTree (dependencyToTree exampleDep2)
 
-postorder :: Tree String -> [String]
-postorder(Node lab kids) = concatMap postorder kids ++ [lab]
+postorder :: Tree a -> [a]
+postorder (Node label kids) = concatMap postorder kids ++ [label]
