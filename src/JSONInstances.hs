7fe2d99fbcba831fa912c599fefe0c8a9187f326
Paul David <paul@denknerd.org>
2017-09-21 23:05:31 +1000

    Make ready for deployment to Hackage.

--- src/JSONInstances.hs
+++ src/JSONInstances.hs
@@ -8,21 +8,21 @@ import Data.Aeson
 import Data.Aeson.Types    -- that's where Parser comes from
 
 instance FromJSON Export where
-  parseJSON = withObject "Export" $ \t -> do
+  parseJSON = withObject "Export" $ \t ->
     Export <$> t .: "ExportName"
 
 instance FromJSON Stacks where
-  parseJSON = withObject "Stacks" $ \t -> do
+  parseJSON = withObject "Stacks" $ \t ->
     Stacks <$> t .: "Stacks"
 
 instance FromJSON Stack where
-  parseJSON = withObject "Stack" $ \t -> do
+  parseJSON = withObject "Stack" $ \t ->
     Stack <$> t .:  "StackId"
           <*> t .:  "StackName"
           <*> t .:? "Outputs" .!= [] -- If there are no outputs, return []
 
 instance FromJSON Imports where
-  parseJSON = withObject "Imports" $ \t -> do
+  parseJSON = withObject "Imports" $ \t ->
     Imports <$> t .: "Imports"
 
 instance FromJSON StackName
