63e8a1933c18253f9137bbab7c9dff9fb7ce8bb4
Paul David <paul@denknerd.org>
2017-09-22 14:53:23 +1000

    Update to Docker CE.

--- update_docker.sh
+++ update_docker.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -cs) main" > /etc/apt/sources.list.d/docker.list'
-curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
-sudo apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+# Remove old versions:
+sudo apt-get remove docker docker-engine docker.io
 sudo apt-get update
-sudo apt-get -y install "docker-engine=17.05.0~ce-0~ubuntu-$(lsb_release -cs)"
+sudo apt-get install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+# APT over HTTPS
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 9DC858229FC7DD38854AE2D88D81803C0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
