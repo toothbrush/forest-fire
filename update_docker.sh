287314a8122b82aecc7eb206f493bd54f2259421
Paul David <paul@denknerd.org>
2017-09-22 14:47:24 +1000

    Update ancient Docker on Travis.

---
+++ update_docker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -cs) main" > /etc/apt/sources.list.d/docker.list'
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+sudo apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get update
+sudo apt-get -y install "docker-engine=17.05.0~ce-0~ubuntu-$(lsb_release -cs)"
